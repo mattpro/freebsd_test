59516e587d9db95dc2d7e89be47b2b411ba92419
mattpro <gancarczyk.mateusz@gmail.com>
2018-12-07 13:17:29 +0000

    Pierwsze proby z spi

--- test.c
+++ test.c
@@ -15,11 +15,29 @@
 
 device_t dev;
 
+void spi_send( uint8_t byte );
+
+void spi_send( uint8_t byte )
+{
+	struct spi_comend spi_cmd;
+	uint8_t temp;
+
+	memset(&spi_cmd, 0, sizeof(struct spi_command) );
+	spi_cmd.tx_data = &byte;
+	spi_cmd.rx_data = &temp;
+	spi_cmd.rx_data_sz = 1;
+	spi_cmd.tx_data_sz = 1;
+	
+	// SPIBUS_TRANSFER(device_get_parent( devSpi ), devSpi, &spi_cmd );
+
+}
+
 
 static int hello_modevent( module_t mod __unused, int event, void *arg __unused)
 {
 	int error = 0;
-	
+	int i;	
+
 	switch (event)
 	{
 		case MOD_LOAD:
@@ -31,8 +49,12 @@ static int hello_modevent( module_t mod __unused, int event, void *arg __unused)
 			}
 
 			GPIO_PIN_SETFLAGS(dev, 26, GPIO_PIN_OUTPUT);
-			GPIO_PIN_SET( dev, 26, GPIO_PIN_HIGH);
-			DELAY(10000);
+			for( i = 0 ; i < 255 ; i ++ )
+			{
+				GPIO_PIN_TOGGLE( dev, 26);
+				DELAY(50000);
+			}
+		
 			GPIO_PIN_SET(dev, 26, GPIO_PIN_LOW);
 
 		break;
